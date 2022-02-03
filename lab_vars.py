b1418a5c021501764f7b1a5b663dccd147df0522
sconrod-tester <sconrod1@gmail.com>
2022-02-03 15:54:12 -0600

    updates

--- lab_vars.py
+++ lab_vars.py
@@ -1,5 +1,5 @@
 name='us-east-2c'
 region='us-east-2'
 az='us-east-2c'
-csr_ami_id='ami-07dde80b3b3ccfb2f'
-ubuntu_ami_id=' ami-0c5db8b1650fb4b47'
+csr_ami_id='ami-0d43ca842a14ff342'
+ubuntu_ami_id='ami-05117a5348bb8a291'
